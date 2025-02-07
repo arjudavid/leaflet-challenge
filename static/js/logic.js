ad7a39075caab3706fbb5c1b2f98242cb018766e
arjudavid <daarju97@gmail.com>
2025-02-06 18:05:56 -0600

    Adding all files from the code into the repo

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,84 +1,119 @@
-// Create the 'basemap' tile layer that will be the background of our map.
+ // Part 1: Step 1: Create the 'basemap' tile layer
+ let basemap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors"
+});
 
+// OPTIONAL: Step 2 - Create a 'street' tile layer
+let streetMap = L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap contributors, Humanitarian OpenStreetMap Team"
+});
 
-// OPTIONAL: Step 2
-// Create the 'street' tile layer as a second background of the map
+// OPTIONAL: Step 3 - Create a 'satelite' title layer
+let satelliteMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  maxZoom: 18,
+  attribution: 'Tiles © Esri'
+});
 
+// Creates a map object with center and zoom options
+let map = L.map("map", {
+  center: [20, -30], 
+  zoom: 3,
+  layers: [basemap] 
+});
 
-// Create the map object with center and zoom options.
+// Layer groups for earthquakes and tectonic plates
+let earthquakes = L.layerGroup();
+let tectonicPlates = L.layerGroup();
 
+// Creates the baseMaps and overlayMaps objects
+let baseMaps = {
+  "Basic Map": basemap,
+  "Street Map": streetMap,
+  "Satellite View": satelliteMap
+};
 
-// Then add the 'basemap' tile layer to the map.
+let overlayMaps = {
+  "Earthquakes": earthquakes,
+  "Tectonic Plates": tectonicPlates
+};
 
-// OPTIONAL: Step 2
-// Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
-// Add a control to the map that will allow the user to change which layers are visible.
+// Adds a layer control to the map
+L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
 
-
-// Make a request that retrieves the earthquake geoJSON data.
+// Fetch Earthquake GeoJSON Data
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
 
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. Pass the magnitude and depth of the earthquake into two separate functions
-  // to calculate the color and radius.
-  function styleInfo(feature) {
-
-  }
-
-  // This function determines the color of the marker based on the depth of the earthquake.
+  // Function to determine color based on depth
   function getColor(depth) {
-
+      return depth > 90 ? "#ff0000" :
+             depth > 70 ? "#ff6600" :
+             depth > 50 ? "#ffcc00" :
+             depth > 30 ? "#ccff33" :
+             depth > 10 ? "#66ff66" :
+                          "#00ff00";
   }
 
-  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Function to determine marker radius based on magnitude
   function getRadius(magnitude) {
-
+      return magnitude * 4;
   }
 
-  // Add a GeoJSON layer to the map once the file is loaded.
+  // Function to style the earthquake markers
+  function styleInfo(feature) {
+      return {
+          radius: getRadius(feature.properties.mag),
+          fillColor: getColor(feature.geometry.coordinates[2]),
+          color: "#000",
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+      };
+  }
+
+  // Adds the GeoJSON layer to the map
   L.geoJson(data, {
-    // Turn each feature into a circleMarker on the map.
-    pointToLayer: function (feature, latlng) {
+      pointToLayer: function (feature, latlng) {
+          return L.circleMarker(latlng);
+      },
+      style: styleInfo,
+      onEachFeature: function (feature, layer) {
+          layer.bindPopup(`
+              <strong>Location:</strong> ${feature.properties.place}<br>
+              <strong>Magnitude:</strong> ${feature.properties.mag}<br>
+              <strong>Depth:</strong> ${feature.geometry.coordinates[2]} km<br>
+          `);
+      }
+  }).addTo(earthquakes);
 
-    },
-    // Set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
-    // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
-    onEachFeature: function (feature, layer) {
+  earthquakes.addTo(map);
 
-    }
-  // OPTIONAL: Step 2
-  // Add the data to the earthquake layer instead of directly to the map.
-  }).addTo(map);
+  // Create legend control
+  let legend = L.control({ position: "bottomright" });
 
-  // Create a legend control object.
-  let legend = L.control({
-    position: "bottomright"
-  });
-
-  // Then add all the details for the legend
   legend.onAdd = function () {
-    let div = L.DomUtil.create("div", "info legend");
+      let div = L.DomUtil.create("div", "info legend"),
+          depths = [-10, 10, 30, 50, 70, 90],
+          colors = ["#00ff00", "#66ff66", "#ccff33", "#ffcc00", "#ff6600", "#ff0000"];
 
-    // Initialize depth intervals and colors for the legend
+      div.innerHTML = "<strong>Depth (km)</strong><br>";
 
-
-    // Loop through our depth intervals to generate a label with a colored square for each interval.
-
-
-    return div;
+      for (let i = 0; i < depths.length; i++) {
+          div.innerHTML +=
+              `<i style="background:${colors[i]}; width:10px; height:10px; display:inline-block;"></i> 
+              ${depths[i]}${depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+'}`;
+      }
+      return div;
   };
 
-  // Finally, add the legend to the map.
+  legend.addTo(map);
+});
 
+// OPTIONAL: Part 2 - Fetch Tectonic Plates GeoJSON Data
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
+  L.geoJson(plate_data, {
+      color: "orange",
+      weight: 2
+  }).addTo(tectonicPlates);
 
-  // OPTIONAL: Step 2
-  // Make a request to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (plate_data) {
-    // Save the geoJSON data, along with style information, to the tectonic_plates layer.
-
-
-    // Then add the tectonic_plates layer to the map.
-
-  });
+  tectonicPlates.addTo(map);
 });
